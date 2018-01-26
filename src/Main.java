425dec91f4f04cbbb6ef32020e761e4215262338
Patrick95 <lawcy@qq.com>
2018-01-27 00:57:58 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    public void run() {
+        // 绘制棋盘
+        // 双方下棋
+        // 触发胜利条件
+    }
+}
