32c672237f8ca9fee7e99e4e918f8e4ffcbed3e6
Patrick95 <lawcy@qq.com>
2018-01-27 01:19:38 +0800

    init

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         new Main().run();
     }
 
     public void run() {
-        // 绘制棋盘
-        // 双方下棋
-        // 触发胜利条件
+        // 玩家信息输入
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入白棋棋手名称：");
+        String whitePLayer = scanner.nextLine();
+        System.out.println("请输入黑棋棋手名称：");
+        String blackPlayer = scanner.nextLine();
+
+        // 绘制初始棋盘
+        drawBoard();
+
+        // 游戏开始
+        int win = 0;
+
+        // Loop
+        do {
+            // 下棋
+            // 绘制棋盘
+            // 检测是否触发胜利条件
+        } while(win == 0);
+        // End Loop
+
+
+    }
+
+    /*
+    绘制棋盘
+     */
+    public void drawBoard() {
+
     }
 }
